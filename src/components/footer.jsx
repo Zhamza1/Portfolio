5ad18c0005d6e632f6c6c28815b001b52996ff41
Hamza Mahmood <hamza.mahmood@outlook.fr>
2022-11-29 22:38:12 +0100

    90

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,8 @@ import {
   Link
 } from "react-router-dom";
 import Logo from "../asset/Logo.svg"
-
+import { AiOutlineWhatsApp } from 'react-icons/ai';
+import { AiFillLinkedin } from 'react-icons/ai';
 
 const footer = (props) => {
   return (
@@ -26,6 +27,10 @@ const footer = (props) => {
 
         <div>
           <p className='text-[2rem] font-normal '>Connect</p>
+          <div className='flex pt-[2rem] ' >
+            <a href="https://www.linkedin.com/in/hamza-mahmood-b19128217/" target="_blank" ><AiFillLinkedin className='w-[3rem] h-[3rem] cursor-pointer' /></a>
+            <a href="tel:+33 7 67 33 69 79" target="_blank" ><AiOutlineWhatsApp  className='w-[3rem] h-[3rem] ml-[1.5rem] cursor-pointer ' /></a>
+          </div>
           <Link to="/contact" > <p className='pt-[2rem] font-light text-[1.6rem]'>Contact</p> </Link>
           <p className='pt-[1rem] font-light cursor-pointer text-[1.6rem] ' >Mentions légales </p>
           </div>
--- src/components/homeCard.jsx
+++ src/components/homeCard.jsx
@@ -5,7 +5,7 @@ const homeCard = (props) => {
   return (
     <>
         <div className={` cursor-pointer font-medium text-[3.6rem] w-[34rem] h-[59rem] rounded-[1rem] xl:w-[28%] xl:h-[70rem] mx-4 bg-no-repeat bg-center ${props.bgImage}`}> 
-          <div className='pt-[4rem] text-center transition transition-opacity-25 backdrop-blur-[0.5rem] bg-[#000000] top-0 left-0 w-full h-full rounded-xl opacity-0 hover:opacity-60 transition-transform-25 hover:translate-y-[0px] translate-y-[2rem] '>
+          <div className='pt-[4rem] text-center transition transition-opacity-25 backdrop-blur-[0.5rem] bg-[#000000] top-0 left-0 w-full h-full rounded-xl opacity-0 hover:opacity-60 transition-transform-25 hover:translate-y-[0px] translate-y-[2rem] px-[2rem] '>
             <div className='text-[#ffffff]' >{props.title}</div>
               <p className='text-[#ffffff] font-regular text-[1.4rem] text-center ' >{props.description}</p>
             
